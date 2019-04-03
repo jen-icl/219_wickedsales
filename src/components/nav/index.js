b38e5ffa987c9352f762db71962285bd218bed26
I Chun Lai <jenniferlai.81@gmail.com>
2019-04-02 18:11:36 -0700

    Main nav in progress

---
+++ src/components/nav/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import Sidenav from './sidenav';
+
+class Nav extends Component {
+    render(){
+        return(
+            <div>
+                <nav className="purple darken-2">
+                    <div className="nav-wrapper">
+                        <Link className="brand-logo" to="/">Wicked Sales</Link>
+                        <a href="#" data-target="sidenav" className="sidenav-trigger">
+                            <i className="material-icons">menu</i>
+                        </a>
+                        <ul class="right hide-on-med-and-down">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/products">Products</Link>
+                            </li>
+                            <li>
+                                <Link to="/">Sign In</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <Sidenav />
+            </div>
+        );
+    }
+}
+
+export default Nav;
