3042e3744aa91dc2c92365e1f775d158aff5e06a
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-04 13:51:56 -0700

    completed product details php added product details dummy data added product details endpoint altered outbound data json encoded data handle errors

--- public/api/data/databasedump.sql
+++ public/api/data/databasedump.sql
@@ -1,9 +1,11 @@
+-- 2019-04-04
+
 -- phpMyAdmin SQL Dump
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 29, 2019 at 08:12 PM
+-- Generation Time: Apr 04, 2019 at 08:32 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
 
@@ -17,55 +19,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `images`
---
-
-CREATE TABLE `images` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `products_id` mediumint(8) UNSIGNED NOT NULL,
-  `url` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `images`
---
-
-INSERT INTO `images` (`id`, `products_id`, `url`) VALUES
-(1, 1, 'images/wickedThings.jpg'),
-(2, 2, 'images/wickedShoes.jpg'),
-(3, 2, 'images/wickedShoes2.jpg'),
-(4, 3, 'images/wickedBrick.jpg');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
   `id` mediumint(8) UNSIGNED NOT NULL,
   `name` varchar(30) NOT NULL,
-  `price` bigint(20) UNSIGNED NOT NULL
+  `price` bigint(20) UNSIGNED NOT NULL,
+  `description` text NOT NULL,
+  `misc_details` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`) VALUES
-(1, 'Wicked Thing', 2000),
-(2, 'Wicked Shoes', 42150),
-(3, 'Wicked Brick', 525);
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `misc_details`) VALUES
+(1, 'Wicked Thing', 2000, 'This product will solve all of your ills, cure famine, and make you look more like bradd pitt, regardless if you are male or female', '{\"height\":\"20cm\",\"width\":\"15cm\",\"weight\":4.2,\"manufacturer\":\"Therbal\"}'),
+(2, 'Wicked Clogs', 42150, 'This is the noisiest shoe you will ever wear.', '{\"availableSizes\":[5,6,7,8,9,10,13,14,17,20],\"colors\":\"red, green, blue, natural\",\"materials\":\"pine, balso\"}'),
+(3, 'Wicked Brick', 525, 'It\'s brick, it\'s brick, it\'s big, it\'s bad.', '{\"height\":\"3 inches\",\"width\":\"3 inches\",\"length\":\"6 inches\",\"colors\":\"red, cherry, maroon, speckled\",\"manufacturer\":\"Riot Brick Studios\",\"weight\":\"8 lbs\",\"courseness\":\"very rough\"}');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -76,12 +52,6 @@ ALTER TABLE `products`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
