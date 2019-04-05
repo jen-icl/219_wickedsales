1565f13e6f2b4a4de53acaa6cc25e18cf801959c
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-04 18:04:49 -0700

    client view product list complete

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -1,5 +1,5 @@
 <?php
-
+    require_once('config.php');
     require_once('mysqlconnect.php');
     require_once('functions.php');
 
---
+++ src/helpers/index.js
@@ -0,0 +1,8 @@
+export function formatMoney(pennies){
+    pennies = parseFloat(pennies);
+    if(isNaN(pennies)){
+        return 'TBA';
+    }
+    const dollars = (pennies / 100).toFixed(2);
+    return `$${dollars}`;
+}
