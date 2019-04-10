98309ed3e01214aed53ddb4f6eac1c66eaf84af0
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-09 17:18:16 -0700

    completed getcartitems endpoint

--- public/api/getcartitems.php
+++ public/api/getcartitems.php
@@ -4,55 +4,55 @@
     require_once('config.php');
     require_once('mysqlconnect.php');
 
-    $cart_id = $_SESSION['cart_id'];
-    //query database to get getcartitems.json
-    $cart_items_query =
-    "SELECT p.`name`, p.`price`, GROUP_CONCAT(i.`url`) AS 'images', c.`quantity`, p.`id` AS 'id'
-    FROM `cart_items` AS c
-    JOIN `products` AS p
-        ON c.`products_id` = p.`id`
-    JOIN `images` AS i
-        ON i.`products_id` = p.`id`
-    WHERE c.`carts_id` = $cart_id
-    GROUP BY p.`id`
-    ";
-
-    $result = mysqli_query($conn, $cart_items_query);
-    if(!$result){
-        throw new Exception(mysqli_error($conn));
-    }
-    if(mysqli_num_rows($result) === 0){
-        throw new Exception('Did not retrieve cart item data');
+    if(empty($_SESSION['cart_id'])){
+        throw new Exception('Missing cart id');
     }
 
-    $get_result = [];
-    while($row = mysqli_fetch_assoc($result)){
-        $get_result[] = $row;
-    };
-
-    $cart_meta_data_query =
-    "SELECT `created`, `total_price`
-    FROM `carts`
-    WHERE `id` = $cart_id
-    ";
-    $meta_data = mysqli_query($conn, $cart_meta_data_query);
-    if(!$meta_data){
-        throw new Exception(mysqli_error($conn));
-    }
-    if(mysqli_num_rows($meta_data) === 0){
-        throw new Exception('Did not retrieve meta data');
-    }
-
-    $get_meta_data = [];
-    while($row = mysqli_fetch_assoc($meta_data)){
-        $get_meta_data[] = $row;
-    }
-
-    $output = [
-        'success' => true,
-        'cartData' => $get_result,
-        'cartMetaData' => $get_meta_data
+    $ouput = [
+        'success' => false,
     ];
-    print(json_encode($output));
 
+    $cart_id = $_SESSION['cart_id'];
+
+    $cart_query =
+    "SELECT c.`created`, c.`total_price`,
+    ci.`quantity`, p.`id`, p.`name`, p.`price`,
+        (SELECT `url`
+        FROM `images`
+        WHERE `products_id` = p.`id`
+        LIMIT 1)
+        AS 'image'
+    FROM `carts` AS c
+    JOIN `cart_items` AS ci
+        ON c.`id` = ci.`carts_id`
+    JOIN `products` AS p
+        ON ci.`products_id` = p.`id`
+    WHERE c.`id` = $cart_id
+    ";
+
+    $cart_data = mysqli_query($conn, $cart_query);
+    if(!$cart_data){
+        throw new Exception(mysqli_error($conn));
+    }
+    if(mysqli_num_rows($cart_data) === 0){
+        throw new Exception('Unable to retrieve cart data');
+    }
+
+    $ouput['cartItems'] = [];
+    $output['cartMetaData'] = [];
+
+    while($row = mysqli_fetch_assoc($cart_data)){
+        $output['cartItems'][] = [
+            'name' => $row['name'],
+            'price' => (int)$row['price'],
+            'image' => $row['image'],
+            'quantity' => (int)$row['quantity'],
+            'id' => (int)$row['id']
+        ];
+        $output['cartMetaData']['created'] = $row['created'];
+        $output['cartMetaData']['total'] = (int)$row['total_price'];
+    }
+    $output['success'] = true;
+
+    print(json_encode($output));
 ?>
