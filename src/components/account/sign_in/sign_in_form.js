547c14a05d18ecee2b5792b73273b8ee39d3b8dc
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-15 18:33:34 -0700

    added Field component sign in form in progress

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
+import Input from '../../general/input';
 
 const SignInForm = props => {
+    console.log('sign in from props', props)
+    const {handleSubmit, signIn} = props;
     return (
-        <form>
-            <h1>Form Goes Here</h1>
+        <form onSubmit={handleSubmit(signIn)}>
+            <div className="row">
+                <Field id="email" col="s12" name="email" component={Input} label="email" />
+            </div>
+            <div className="row">
+                <Field id="password" col="s12" name="password" component={Input} type="password" label="password" />
+            </div>
+            <div className="row">
+                <div className="col s12 right-align">
+                    <button className="btn red darken-2">Sign In</button>
+                </div>
+            </div>
         </form>
     );
 }
+
+//<Field id="email" col="s12" name="email" component={Input} label="email" />
+//default props are name and component
+//component prop accepts a component for the input field layout, refer to input.js
 
 export default reduxForm({
     form: 'sign-in-form' //sets a unique name for the form to keep track of this form
