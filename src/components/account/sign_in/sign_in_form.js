480a03b1efefe8895e794b071c99b8b401269ca2
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-16 17:57:08 -0700

    sign in validation and sign up form completed

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -8,10 +8,10 @@ const SignInForm = props => {
     return (
         <form onSubmit={handleSubmit(signIn)}>
             <div className="row">
-                <Field id="email" col="s12" name="email" component={Input} label="email" />
+                <Field id="email" col="s12" name="email" component={Input} label="Email" type="email" />
             </div>
             <div className="row">
-                <Field id="password" col="s12" name="password" component={Input} type="password" label="password" />
+                <Field id="password" col="s12" name="password" component={Input} label="Password" type="password" />
             </div>
             <div className="row">
                 <div className="col s12 right-align">
@@ -26,10 +26,23 @@ const SignInForm = props => {
 //default props are name and component
 //component prop accepts a component for the input field layout, refer to input.js
 
+function validate({ email, password }){
+    const errors = {};
+    if(!email){
+        errors.email = 'Please enter your email'; //property name must be the same value as the name prop in the input
+    }
+    if(!password){
+        errors.password = 'Please enter your password';
+    }
+    return errors; //validation must return an object
+}
+//validation will assign error beneath the input area where the error exists
+
 export default reduxForm({
-    form: 'sign-in-form' //sets a unique name for the form to keep track of this form
+    form: 'sign-in-form', //sets a unique name for the form to keep track of this form
+    validate: validate //must use validate as key name, function name can be named anything (doesn't have to be called validate)
 })(SignInForm);
 
 //reduxForm() returns a function
-//param: enter a config object, name form by entering any name to the form property
+//param: enter a config object, name form by entering any name to the form property (form key)
 //() calls the function returned by the redux form function call
